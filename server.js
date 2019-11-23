a93a305dbcd556d93415560ec0c2ae9eaaba817c
Raahim Salman <raahimsal@gmail.com>
2019-11-23 14:33:40 -0500

    fin fin

---
+++ server.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+
+axios({method: 'POST', url: 'https://trackapi.nutritionix.com/v2/natural/nutrients', 
+headers: {
+    'x-app-id' : "ac141a81", 
+    'x-app-key' : "b2bb976cbf044a6f0a10a817debb5243",
+    },
+data: {
+    "query":"banana",
+"locale": "en_US"
+    }})
+
+.then((res) => {
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(res.data.foods[0])
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+  
